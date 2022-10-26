28c27964a3d80c670df05eaecfa1a282cd3f0c23
longf <longf@DESKTOP-2GJ1G8J>
2022-10-26 20:38:44 +0200

    Add project files.

---
+++ Uppgift 4.23/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Uppgift_4_23
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Skriv in ett heltal");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int störst=x;
+
+            string svar = "";
+            while (svar != "n")
+            {
+                Console.WriteLine("vill du skriv in ett heltal till? (j/n)");
+                svar = Console.ReadLine();
+
+                if (svar == "n")
+                {
+                    break;
+                }
+                Console.WriteLine("skriv in ett heltal till");
+
+                int nytttal = int.Parse(Console.ReadLine());
+
+                if (nytttal > störst)
+                {
+                    störst = nytttal;
+
+                }
+
+            }
+            Console.WriteLine("Det största talet som skrevs var " + störst);
+        }
+    }
+}
